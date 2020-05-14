05e21b9f67304f5296305a7f3617bfc0cfeaf818
kamils224 <kamil.sagalara@student.put.poznan.pl>
2020-05-14 18:02:15 +0200

    task2 menu toggle and animation

--- Task1&2/index.js
+++ Task1&2/index.js
@@ -1,12 +1,24 @@
-function parallax(){
+$(document).ready(function(){
+    toggleMenu();
+})
 
-    window.addEventListener('scroll', (e)=>{
-        var scrolled = window.pageYOffset;
-        const background = document.querySelector('.parallax');
-        background.style.top = -(scrolled * 0.2) + 'px';
-    });
 
+
+
+toggleMenu = function(){
+
+    var hideMenu = true;
+    var topValue = $('#main_navigation').position().top;
+    const showMenuHeight = 570;
+    console.log(topValue); 
+    $('.show-menu').on('click',function(){
+        console.log('click');
+        if(hideMenu === false){
+            $('#main_navigation').animate({top:topValue},700);
+            hideMenu = true;
+        }else{
+            $('#main_navigation').animate({top: showMenuHeight},700);
+            hideMenu = false;
+        }
+    })
 }
-
-parallax();
-
